0706750e6f3cf8e59d4f90432d0fbbc878a3452a
Michal Kowol <michal.kowol@gmail.com>
2015-11-27 13:47:04 +0100

    NT. Fixes for win.

--- src/main/scala/pl/yourcode/photocomparer/Boot.scala
+++ src/main/scala/pl/yourcode/photocomparer/Boot.scala
@@ -2,10 +2,10 @@ package pl.yourcode.photocomparer
 
 object Boot extends Logging {
   def main(args: Array[String]): Unit = {
-    val p1 = "/Users/kowolm/Desktop/Zdjecia i Filmy/Slub Nowaka"
-    val p2 = "/Users/kowolm/Desktop/Zdjecia i Filmy/sss"
+    val p1 = """C:\Users\michal\Desktop\Nowy folder (2)\b"""
+    val p2 = """C:\Users\michal\Desktop\Nowy folder (2)\a"""
     val duplicates = FileDeduplicator(p1).duplicates(p2)
     duplicates.foreach(println)
-    FileMover.move("/Users/kowolm/test", duplicates)
+    FileMover.move("""C:\Users\michal\Desktop\Nowy folder (2)\c""", duplicates)
   }
 }
--- src/main/scala/pl/yourcode/photocomparer/FileMover.scala
+++ src/main/scala/pl/yourcode/photocomparer/FileMover.scala
@@ -7,7 +7,7 @@ import com.google.common.io.Files
 object FileMover {
   def move(moveToDirectoryName: String, files: Seq[File]): Unit = {
     files.foreach { file =>
-      val moveTo = new File(moveToDirectoryName + file.getCanonicalPath)
+      val moveTo = new File(moveToDirectoryName + file.getCanonicalPath.replaceAll("""^.:\\""", """\\"""))
       Files.createParentDirs(moveTo)
       Files.move(file, moveTo)
     }
